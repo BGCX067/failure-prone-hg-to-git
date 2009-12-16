feeba071b6ad53b486952f5acecf47f238aa3df8
root <root@icelus>
2009-12-16 18:06:12 -0400

    modificado o main para suportar a janela e eventos

--- src/main.c
+++ src/main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+#include "glapp.h"
+
+int idle(void* a){
+	return 1;
+}
+
+int render(void* a){
+	return 1;
+}
 int main(){
 
+	glapp* app = setVideoMode(800, 600, 0);
+	if (!app){
+		printf("Invalid Video Mode\n");
+		return 1;
+	}else{
+		printf("Video mode: w:  %d h: %d depth: %d \n", app->width, app->height, app->depth);
+	}
+
+	keyboard key;
+	mouse m;
+	mainloop(app, key, m, idle, render );
+
+	closeVideo();
+	free(app);
 	return 0;
 }
