11b48e0e6ea8387de5f53d5d3877ef31f1c7600b
abnern <devnull@localhost>
2012-05-05 21:35:59 -0300

    - comeco do arquivo que detalha o mapa

--- tools/glowfalls/src/main.c
+++ tools/glowfalls/src/main.c
@@ -7,6 +7,7 @@
 #include "renderer/camera.h"
 #include "chipmunk/chipmunk.h"
 #include "util/fparray.h"
+#include "util/ezxml.h"
 
 #define GRABABLE_MASK_BIT (1<<31)
 #define NOT_GRABABLE_MASK (~GRABABLE_MASK_BIT)
@@ -33,6 +34,10 @@ typedef struct gameObject {
 	cpShape* shape;	
 	sprite* gfx;
 	int layer;
+
+	//a geometria de colisao dica em shape, mas as vezes o drawrect eh maior ou menor que a geometria
+	rect drawrect;
+	cpVect colisionrect[4];
 
 } GameObject;
 
@@ -98,25 +103,77 @@ playerUpdateVelocity(cpBody *body, cpVect gravity, cpFloat damping, cpFloat dt)
 	body->v.x = cpfclamp(body->v.x, -400, 400);
 }
 
+int loadStage(char* filename){
 
-void InitializeGame(){
+	ezxml_t stage = ezxml_parse_file(filename);
+	if (!stage){
+		printf("File not found: %s \n", filename);
+		return 0;
+	}
+
+
+	ezxml_t object;
+
+	cpBody *body, *staticBody = &space->staticBody;
 
 	gameobjects = fplist_init( free);
+	for (object = ezxml_child(stage, "object" ); object; object = object->next ){
+		//cria o objeto
+		GameObject* g1 = malloc(sizeof(GameObject));
+		g1->gfx = initializeSprite();
+		g1->layer = atoi(ezxml_attr(object, "layer"));
+		ezxml_t frame;
+		//primeiro adiciona todos os sprites dele
+		for ( frame = ezxml_child(object, "frame"); frame; frame = frame->next){
+			int numimages =  atoi( ezxml_attr(frame, "numimages") );
+			float delay = atof( ezxml_attr(frame, "delay") );
 
-	s = initializeSprite();
-	addSprites(s, "dude/run%d.png", 27, 0.03);
-	addSprites(s, "dude/jumpprep%d.png", 5, 0.1);
-	addSprites(s, "dude/idle%d.png", 22, 0.1);
+			if ( numimages == 1 ){
+				addSprite(g1->gfx, frame->txt, 0 );
+			}else {
+				addSprites(g1->gfx, frame->txt,numimages ,delay );
+			}
+		}
 
-	GameObject* g1 = malloc(sizeof(GameObject));
-	grass = initializeSprite();
-	if (addSprite(grass, "data/sprites/longgrass_1.png", 0) == 0)
-		printf("grass sprite not found \n");
+		ezxml_t rect = ezxml_child(object, "rect");
+		if (rect){
+			cpShape* shape;
+			//cpVect *v = sizeof(cpVect)*4;
+			float x = atof(ezxml_attr(rect, "x"));
+			float y = atof(ezxml_attr(rect, "y"));
+			float sizex = atof(ezxml_attr(rect, "sizex"));
+			float sizey = atof(ezxml_attr(rect, "sizey")); 
+			g1->colisionrect[0].x = x; g1->colisionrect[0].y = y+sizey;
+			g1->colisionrect[1].x = x+sizex; g1->colisionrect[1].y = y+sizey;
+			g1->colisionrect[2].x = x+sizex; g1->colisionrect[2].y = y;
+			g1->colisionrect[3].x = x; g1->colisionrect[3].y = y;		
+			shape = cpSpaceAddShape(space, cpPolyShapeNew(staticBody, 4,  g1->colisionrect, cpvzero ) );
+			shape->e = 1.0f; shape->u = 1.0f;
+			shape->layers = NOT_GRABABLE_MASK;
+			shape->collision_type = 2;
+			g1->shape = shape;
+		}
 
-	g1->gfx = grass;
-	g1->layer = 0;
-	fplist_insback(g1, gameobjects);
+		ezxml_t drect = ezxml_child(object, "drawrect");
+		if (drect){
+			float x = atof(ezxml_attr(drect, "x"));
+			float y = atof(ezxml_attr(drect, "y"));
+			float sizex = atof(ezxml_attr(drect, "sizex"));
+			float sizey = atof(ezxml_attr(drect, "sizey"));
+			g1->drawrect.x = x;
+			g1->drawrect.y = y;
+			g1->drawrect.w = sizex;
+			g1->drawrect.h = sizey;
+		}
 
+		fplist_insback(g1, gameobjects);
+		
+	}
+	ezxml_free(stage); 
+	return 1;
+}
+
+void InitializeGame(){
 
 	cpInitChipmunk();
 	cpResetShapeIdCounter();
@@ -124,6 +181,22 @@ void InitializeGame(){
 	space = cpSpaceNew();
 	space->iterations = 10;
 	space->gravity = cpv(0, -1500);
+
+
+	if (loadStage("data/stagetest.xml") == 0)
+		return;
+
+	
+
+	s = initializeSprite();
+	addSprites(s, "dude/run%d.png", 27, 0.03);
+	addSprites(s, "dude/jumpprep%d.png", 5, 0.1);
+	addSprites(s, "dude/idle%d.png", 22, 0.1);
+
+//	if (addSprite(grass, "data/sprites/longgrass_1.png", 0) == 0)
+//		printf("grass sprite not found \n");
+
+
 
 	cpBody *body, *staticBody = &space->staticBody;
 	cpShape *shape;
@@ -136,11 +209,11 @@ void InitializeGame(){
 
 	
 	// add some other segments to play with
-	shape = cpSpaceAddShape(space, cpSegmentShapeNew(staticBody, cpv(0,100), cpv(800,100), 0.0f));
-	shape->e = 1.0f; shape->u = 1.0f;
-	shape->layers = NOT_GRABABLE_MASK;
-	shape->collision_type = 2;
-	g1->shape = shape;
+	//shape = cpSpaceAddShape(space, cpSegmentShapeNew(staticBody, cpv(0,100), cpv(800,100), 0.0f));
+	//shape->e = 1.0f; shape->u = 1.0f;
+	//shape->layers = NOT_GRABABLE_MASK;
+	//shape->collision_type = 2;
+//	g1->shape = shape;
 
 	/*shape = cpSpaceAddShape(space, cpSegmentShapeNew(staticBody, cpv(0,220), cpv(500,220), 0.0f));
 	shape->e = 1.0f; shape->u = 1.0f;
@@ -313,12 +386,28 @@ int Render(event *e, double* dt){
 
 		for( int i = 0; i < gameobjects->size; i++){
 			GameObject* obj = fplist_getdata(i, gameobjects);
-			drawSprite(obj->gfx, 0, 10, 800, 100, 0, 0, REPEAT_LAST);
+			if (obj->layer == -1)
+				drawSprite(obj->gfx, obj->drawrect.x, obj->drawrect.y, obj->drawrect.w, obj->drawrect.h, 0, 0, REPEAT_LAST);
 		}
+
+		for( int i = 0; i < gameobjects->size; i++){
+			GameObject* obj = fplist_getdata(i, gameobjects);
+			if (obj->layer == 0)
+				drawSprite(obj->gfx, obj->drawrect.x, obj->drawrect.y, obj->drawrect.w, obj->drawrect.h, 0, 0, REPEAT_LAST);
+		}
+
+		cpSpaceHashEach(space->activeShapes, (cpSpaceHashIterator)drawObject, space);
+
+
+		for( int i = 0; i < gameobjects->size; i++){
+			GameObject* obj = fplist_getdata(i, gameobjects);
+			if (obj->layer == 1)
+				drawSprite(obj->gfx, obj->drawrect.x, obj->drawrect.y, obj->drawrect.w, obj->drawrect.h, 0, 0, REPEAT_LAST);
+		}
+
 
 //		drawSprite(grassObject.gfx, 0, 10, 800, 100, 0, 0, REPEAT_LAST);
 
-		cpSpaceHashEach(space->activeShapes, (cpSpaceHashIterator)drawObject, space);
 		//cpSpaceHashEach(space->staticShapes, (cpSpaceHashIterator)drawObject, space);
 	end2d();
 
